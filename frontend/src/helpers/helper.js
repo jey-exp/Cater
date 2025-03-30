608d4b5260bf0afcd769d7fc02c7263dddccac37
jey-exp <jeyshreemen.info@gmail.com>
2025-03-30 22:37:43 +0530

    q added basic supabase integration and zustand

--- frontend/src/helpers/helper.js
+++ frontend/src/helpers/helper.js
@@ -1,15 +1,16 @@
-import jwt from "jsonwebtoken"
+import CryptoJS from 'crypto-js';
 
-export const hash = async(value)=>{
-    const hashed = await jwt.sign(value, process.env.REACT_APP_JWT_SECRET);
-    return hashed;
-}
+export const hash = async (value) => {
+    const secretKey = process.env.REACT_APP_JWT_SECRET;
+    const encrypted = CryptoJS.AES.encrypt(JSON.stringify(value), secretKey).toString();
+    return encrypted;
+};
 
-export const decode = async(hash)=>{
-    const decoded = await jwt.verify(hash, process.env.REACT_APP_JWT_SECRET);
-    const rehash = await jwt.decode(hash);
+export const decode = async (hash) => {
+    const secretKey = process.env.REACT_APP_JWT_SECRET;
+    const bytes = CryptoJS.AES.decrypt(hash, secretKey);
+    const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+    const decoded = JSON.parse(decrypted);
     console.log(decoded);
-    console.log(rehash);
     return decoded;
-}
-
+};
